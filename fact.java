8e4874f016a968af2c9a8be52721f47d9412c126
experionann <ann.davis@experionglobal.com>
2016-10-27 13:04:44 +0530

    hello

---
+++ fact.java
@@ -0,0 +1,14 @@
+import java.io.*;
+class Fact{
+public static void main(string args[])
+ {int f=1,n=4;
+ System.out.println("Enter a value:");
+ for(i=1;i<=n;I++)
+ {f=f*i;}
+System.out.println("fact is"+f);
+ 
+}
+
+}
+
+
